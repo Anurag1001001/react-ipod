e59cfe2de0c5ab57ffc04a4819dd46b00ea2d0e9
anurag1001001 <anuragtripathi23may@gmail.com>
2021-01-19 15:42:00 +0530

    Artists component Done

---
+++ src/components/Screens/Artists.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Artists extends React.Component {
+  render() {
+    return (
+      <div className="artists-screen">
+        <h1>Artists</h1>
+        <div>
+          <i class="fas fa-album"></i>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Artists;
